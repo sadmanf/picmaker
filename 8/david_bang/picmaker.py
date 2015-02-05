22698737dacca53266d1a4376d4ad6bbcb2b563e
David <davidbang2012@gmail.com>
2015-02-05 14:07:18 -0500

    picmaker

--- 8/david_bang/picmaker.py
+++ 8/david_bang/picmaker.py
@@ -11,8 +11,22 @@ for i in range (600):
         blue = str(random.randrange(0,255))
         swag.write ( red + " " + green + " " + blue + "\n")
 
+swag.close()
 
+swag = open ('picmaker2.ppm', 'w')
+swag.write ("P3\n600 600\n255\n")
 
+red = random.randrange(0,255)
+green = random.randrange(0,255)
+blue = random.randrange(0,255)
 
+for i in range (600):
+    if (i > 100):
+        red = red/1.1
+        green = green/1.1
+        blue = green/1.1
+    for j in range (600):
+        swag.write ( str(red) + " " + str(green) + " " + str(blue) + "\n")
 
 swag.close()
+

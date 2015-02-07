3754c756dbb3ced25314bfdcb7c353fed2d4e263
ericwongg <wonggeric@gmail.com>
2015-02-07 13:14:52 -0500

    Trippy picture

--- 8/eric_wong/picmaker.py
+++ 8/eric_wong/picmaker.py
@@ -6,7 +6,7 @@ res.write("255 \n")
 for x in range(512):
     for y in range(512):
         r = x % 255
-        g = x + y % 255
+        g = x % 255
         b = x * y % 255
         res.write("%d %d %d " % (r, g, b))
     res.write("\n")

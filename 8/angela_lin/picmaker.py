c91629927083117dd8e92b5bb0e1266ce8f3af39
Angela Lin <angela97lin@gmail.com>
2015-02-07 22:05:05 -0500

    yey

--- 8/angela_lin/picmaker.py
+++ 8/angela_lin/picmaker.py
@@ -1,16 +1,19 @@
-import random
+#Work 0: Imagine all the pixels...
+#Creating a computer-generated image!
+import random, math
 
-i_width = 500;
-i_height = 500;
+i_width = 600;
+i_height = 600;
 max_color = 255;
 
-def randpicMaker():
-    f = open("randpic.ppm", "w")
+#creates a picture with randomly colored pixels (with max red value to give the pixels a reddish hue)!
+def rand_maker():
+    f = open("rand.ppm", "w")
     header = "P3 %d %d %d \n" % (i_width, i_height, max_color)
     #print header
     f.write(header)
-    for x in range(0, i_width):
-        for y in range(0, i_height):
+    for x in range(i_width):
+        for y in range(i_height):
             red_val = random.randint(0, 255)
             blue_val = random.randint(0, 255)
             green_val = random.randint(0, 255)
@@ -19,35 +22,43 @@ def randpicMaker():
         f.write("\n")
     f.close()
 
-def picMaker():
-    f = open("pic.ppm", "w")
+#makes black swirly image
+def swirl_maker():
+    f = open("swirl.ppm", "w")
     header = "P3 %d %d %d \n" % (i_width, i_height, max_color)
     #print header
     f.write(header)
-    for x in range(0, i_width):
-        for y in range(0, i_height):
-            if (y < 50):
-                red_val = (50 + x) % 255  
-                blue_val = 255
-                green_val = (30 + x) % 255
-            elif (y < 100):
-                red_val = 255 
-                blue_val = (50 + x) % 255
-                green_val = (30 + x) % 255
-            elif (y < 150):    
-                red_val = (50 + x) % 255  
-                blue_val = 255
-                green_val = (30 + x) % 255
-            else:
-                red_val = (50 + x) % 255  
-                blue_val = 255
-                green_val = (30 + x) % 255
-           
+    freq = 1
+    for x in range(i_width):
+        freq = freq + .05
+        for y in range(i_height):
+            red_val = abs(math.sin(freq * y)) * 100
+            blue_val = abs(math.sin(freq * y)) * 100
+            green_val = abs(math.sin(freq * y)) * 100
             color = "%d %d %d " % (red_val, green_val, blue_val)
-            #print color
             f.write(color)
         f.write("\n")
     f.close()
-    
+
+#makes a fadey-rainbow image
+def rainbow_maker():
+    f = open("rainbow.ppm", "w")
+    header = "P3 %d %d %d \n" % (i_width, i_height, max_color)
+    #print header
+    f.write(header)
+    freq = .0085
+    for x in range(i_width):
+        #freq = freq + .055 #for swirls
+        for y in range(i_height):
+            red_val = abs(math.sin(freq * y)) * 127 + 128
+            blue_val = abs(math.sin(freq * y +  2*(math.pi/3))) * 127 + 128
+            green_val = abs(math.sin(freq * y +  4*(math.pi/3))) * 127 + 128
+            color = "%d %d %d " % (red_val, green_val, blue_val)
+            f.write(color)
+        f.write("\n")
+    f.close()
+
 if __name__ == "__main__":
-    picMaker()
+    rand_maker()
+    swirl_maker()
+    rainbow_maker()

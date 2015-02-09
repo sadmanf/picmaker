40173dc4fccaa08743926311a7862b2f93a29f6a
Justin Kim <yulli@yulli.org>
2015-02-09 10:05:09 -0500

    ¯\_(ツ)_/¯

--- 8/justin_kim/main.rb
+++ 8/justin_kim/main.rb
@@ -7,19 +7,19 @@ OUTPUT_FILENAME = 'output.ppm'
 
 def red(x, y)
   s = 3.0 / (y + 99)
-  y = (y+Math.sin((x*x+((y-700)**2)*5)/100.0/DIM)*35)*s;
+  y = (y+Math.sin((x*x+((y-700)**2)*5)/100.0/DIM)*35)*s
   ((((x + DIM) * s + y) % 2).to_i + (((DIM * 2 - x) * s + y) % 2).to_i) * 127
 end
 
 def green(x, y)
   s = 3.0 / (y + 99)
-  y = (y+Math.sin((x*x+((y-700)**2)*5)/100.0/DIM)*35)*s;
+  y = (y+Math.sin((x*x+((y-700)**2)*5)/100.0/DIM)*35)*s
   ((10 * ((x + DIM) * s + y) % 2).to_i + (10 * ((DIM * 2 - x) * s + y) % 2).to_i) * 127
 end
 
 def blue(x, y)
   s = 3.0 / (y + 99)
-  y = (y+Math.sin((x*x+((y-700)**2)*5)/100.0/DIM)*35)*s;
+  y = (y+Math.sin((x*x+((y-700)**2)*5)/100.0/DIM)*35)*s
   ((25 * ((x + DIM) * s + y) % 2).to_i + (25 * ((DIM * 2 - x) * s + y) % 2).to_i) * 127
 end
 

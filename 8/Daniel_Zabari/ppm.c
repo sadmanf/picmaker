e948611335c5e90da9d145b2f88df51499c1a3db
Zabari <Zabari.daniel@Gmail.com>
2015-02-09 01:26:39 -0500

    I don't know how to do math

--- 8/Daniel_Zabari/ppm.c
+++ 8/Daniel_Zabari/ppm.c
@@ -3,32 +3,48 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <string.h>
+#include <math.h>
 int main(){
 	int fd=open("yo.ppm", O_CREAT|O_WRONLY, 0644); //the file
-	int maxx=100; //max x value
-	int maxy=100; //max y value
+	int maxx=500; //max x value
+	int maxy=500; //max y value
 	int maxc=255; //max color value
 	char s[10000]="P3\0";
-	char temp[50];
+			//char temp[50];
 	sprintf(s+strlen(s)," %d",maxx);
 	sprintf(s+strlen(s)," %d",maxy);
-	sprintf(s+strlen(s)," %d",maxc);
-	int r=200;
-	int g=100;
-	int b=0;
+	sprintf(s+strlen(s)," %d\n",maxc);
+	write(fd,&s,strlen(s));
+	int r=0;
+	int g=0;
+	int b=55;
 	int i;
-	for (i=maxx; i >= 0; i--){
-		if (r > 256)
-			r=0;
-		if (b > 256)
-			b=0;
-		if (g > 256)
-			g=0;
-		sprintf(s+strlen(s)," %d",r);
-		sprintf(s+strlen(s)," %d",g);
-		sprintf(s+strlen(s)," %d",b);
+	int x=1;
+	int y=1;
+	for (y=maxy/2; y > -1 * maxy/2; y--){
+		for (x=maxx/2; x> -1 * maxx/2; x--){
+				b=0;
+				r=0;
+				g=0;	
+				//b%=maxc;
+				//r=maxc-b;
+				//g=99;
+				//if ((pow((y-3),2)+pow((x-3),2))==9)
+				
+				if (y>0 && (abs(pow(y,1.0/2)-x)<2 || abs(pow(y,1.0/2)+x)<2))
+					r=255;
+				if (pow(pow(x,2)+pow(y,2),1.0/2)<=144 && pow(pow(x,2)+pow(y,2), 1.0/2)>=81)
+					b=255;
+				if (y==(int)(20*sin(x)))
+					g=255;
+				sprintf(s,"%d",r);
+				sprintf(s+strlen(s)," %d",g);
+				sprintf(s+strlen(s)," %d\n",b);
+				write(fd,&s,strlen(s));
+				//printf("%d\n",b);
+		}
 	}
-	write(fd, &s, strlen(s));
+	//write(fd, &s, strlen(s));
+	//printf("%d\n",strlen(s));
 	close(fd);
 }
-

90bffb2d86726b9a6b5d5ae2d401059cdffd5bb5
David Bang <davidbang2012@gmail.com>
2015-02-08 23:26:03 -0500

    done with picmaker

--- 8/david_bang/picmaker.py
+++ 8/david_bang/picmaker.py
@@ -2,7 +2,7 @@ import math,random
 from random import randint
 
 
-swag = open ('picmaker.ppm', 'w')
+swag = open ('picmaker.ppm', 'w') # random pixels mashed together
 swag.write ("P3\n600 600\n255\n")
 
 for i in range (600):
@@ -14,23 +14,23 @@ for i in range (600):
 
 swag.close()
 
-swag = open ('picmaker2.ppm', 'w')
+
+swag = open ('picmaker2.ppm', 'w') # algorithmic art, image is random pattern (sometimes looks ugly, sometimes looks really nice)
 swag.write ("P3\n600 600\n255\n")
 
 red = random.randrange(0,255)
 green = random.randrange(0,255)
 blue = random.randrange(0,255)
 
+
 for i in range (600):
-    if ( i % 2 == 0):
-        red = red + randint(0,5) % 255
-    if ( i % 3 == 0):
-        green = green + randint(0,5) % 255
-    if ( i % 5 == 0):
-        blue = blue + randint(0,5) % 255
     for j in range (600):
         
-        swag.write ( str(red) + " " + str(green) + " " + str(blue) + "\n")
+        r = round(math.fabs(math.sin(i*j*3))* red)
+        g = round(math.fabs(math.sin(i*j*2))* green )
+        b = round(math.fabs(math.sin(i*j*4))* blue)
+
+        swag.write ( str(r) + " " + str(g) + " " + str(b) + "\n")
 
 swag.close()
 
